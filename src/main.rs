843c6b9a9877b781d4dfd41e4516bac05384c474
Н. К. Демирев <nikdem00@gmail.com>
2021-10-03 21:40:22 +0300

    InitFailed

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
-use vulkano::command_buffer::{AutoCommandBufferBuilder, DynamicState, SubpassContents}; 
+use vulkano::command_buffer::{AutoCommandBufferBuilder, DynamicState, SubpassContents, CommandBufferUsage}; 
 use vulkano::device::{Device, DeviceExtensions, physical::PhysicalDevice}; 
 use vulkano::render_pass::{Framebuffer, FramebufferAbstract, RenderPass, Subpass}; 
-use vulkano::image::{SwapchainImage, view::{ImageView,ImageViewAbstract}}; 
+use vulkano::image::{SwapchainImage, view::ImageView}; 
 use vulkano::instance::{Instance}; 
-use vulkano::pipeline::{viewport::Viewport, GraphicsPipeline, GraphicsPipelineBuilder, vertex::BuffersDefinition}; 
+use vulkano::pipeline::{viewport::Viewport, GraphicsPipeline}; 
 use vulkano::swapchain::{AcquireError, PresentMode, SurfaceTransform, Swapchain, SwapchainCreationError}; 
 use vulkano::swapchain; 
 use vulkano::sync::{GpuFuture, FlushError}; 
 use vulkano::sync; 
-use vulkano::{SafeDeref};
 use vulkano_win::VkSurfaceBuild; 
 use winit::window::{WindowBuilder, Window}; 
-use winit::event_loop::{EventLoop, ControlFlow, EventLoopWindowTarget}; 
+use winit::event_loop::{EventLoop, ControlFlow}; 
 use winit::event::{Event, WindowEvent}; 
 use vulkano::buffer::{cpu_access::CpuAccessibleBuffer, BufferUsage};
 use std::sync::Arc; 
-
-
+use std::option::Option;
 
 fn main() 
 {
     // Создаем инстанс
-    let _instance = 
-    { 
+    let _instance = { 
         let extensions = vulkano_win::required_extensions();
         Instance::new(None, vulkano::Version::V1_1, &extensions, None).unwrap() 
     };
@@ -32,26 +29,25 @@ fn main()
 
     // Создаем окно
     let event_loop = EventLoop::new();
-
-    let window = WindowBuilder::new();
-
-    let surface: Arc<swapchain::Surface<Window>> = VkSurfaceBuild::build_vk_surface(window, &event_loop, _instance.clone()).unwrap();
+    let surface: Arc<swapchain::Surface<Window>> = VkSurfaceBuild::build_vk_surface(
+        WindowBuilder::new(), 
+        &event_loop, 
+        _instance.clone()
+    ).unwrap();
 
     // Создадим queue_family для дальнейшего создания экземпляров Queue
-    let queue_family = physical_device
-                                    .queue_families()
-                                    .find(|&q| { 
-                                                  q.supports_graphics() && surface.is_supported(q).unwrap_or(false) 
-                                                }).unwrap();
+    let queue_family = physical_device.queue_families().find(|&q| { 
+            q.supports_graphics() && surface.is_supported(q).unwrap_or(false) 
+    }).unwrap();
 
     // Создаем экземпляр Device для отправки привязки нашей очереди к ГПУ
     let device_ext = DeviceExtensions { khr_swapchain: true, .. DeviceExtensions::none() }; 
     let (device, mut queues) = Device::new(
-                                            physical_device, 
-                                            physical_device.supported_features(), 
-                                            &device_ext, 
-                                            [(queue_family, 0.5)].iter().cloned()
-                                           ).unwrap();
+        physical_device, 
+        physical_device.supported_features(), 
+        &device_ext, 
+        [(queue_family, 0.5)].iter().cloned()
+    ).unwrap();
     
     // Создадим очередь
     let queue = queues.next().unwrap();
@@ -174,29 +170,127 @@ fn main()
             Vertex 
             {
                 position: [-0.5, 0.5, 0.0],
-                col : [0,0,0,0]
+                col : [0.0,0.0,0.0,0.0]
             },
 
             Vertex
             {
                 position: [0.5, 0.5, 0.0],
-                col : [0,0,0,0]
+                col : [0.0,0.0,0.0,0.0]
             }, 
 
             Vertex
             {
                 position: [0.0, -0.5, 0.0],
-                col : [0,0,0,0]
+                col : [0.0,0.0,0.0,0.0]
             }
         ].iter().cloned()
     ).unwrap();
 
     let mut framebuffers = window_size_dependent_setup(&images, render_pass.clone(), &mut dynamic_state);
 
-    
+    let mut recreate_swapchain = false;
+
+    let mut previous_frame_end = Some( 
+        Box::new(
+            sync::now(device.clone())
+        ) as Box<dyn GpuFuture>
+    );
+
+    event_loop.run(move |event, _, control_flow| 
+        {
+            match event
+            { 
+                Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => { 
+                    *control_flow = ControlFlow::Exit; 
+                },
+                
+                Event::WindowEvent { event: WindowEvent::Resized(_), .. } => { 
+                    recreate_swapchain = true; 
+                },
+
+                Event::RedrawEventsCleared => 
+                { 
+                    previous_frame_end.as_mut().take().unwrap().cleanup_finished();
+
+                    if recreate_swapchain 
+                    { 
+                        let dimensions: [u32; 2] = surface.window().inner_size().into();
+                        let (new_swapchain, new_images) = match swapchain.recreate().dimensions(dimensions).build()
+                        { 
+                            Ok(r) => r, 
+                            Err(SwapchainCreationError::UnsupportedDimensions) => return, 
+                            Err(e) => panic!("Failed to recreate swapchain: {:?}", e) 
+                        };
+
+                        swapchain = new_swapchain; 
+                        framebuffers = window_size_dependent_setup(&new_images, render_pass.clone(), &mut dynamic_state); 
+                        recreate_swapchain = false; 
+                    }
+                    
+                    let (image_num, suboptimal, acquire_future) = match swapchain::acquire_next_image(swapchain.clone(), None)
+                    { 
+                        Ok(r) => r, 
+                        Err(AcquireError::OutOfDate) => 
+                        { 
+                            recreate_swapchain = true;
+                            return; 
+                        },
+                        Err(e) => panic!("Failed to acquire next image: {:?}", e) 
+                    };
+
+                    if suboptimal 
+                    { 
+                        recreate_swapchain = true; 
+                    }
+
+                    let clear_values = vec!([0.0, 0.0, 0.0, 1.0].into());
+
+                    let mut cmd_buffer_builder = AutoCommandBufferBuilder::primary(device.clone(), queue.family(), CommandBufferUsage::OneTimeSubmit).unwrap();
+                    cmd_buffer_builder
+                        .begin_render_pass(framebuffers[image_num].clone(), SubpassContents::Inline, clear_values)
+                        .unwrap()
+                        .draw(pipeline.clone(), &dynamic_state, vertex_buffer.clone(), (), ())
+                        .unwrap()
+                        .end_render_pass()
+                    .unwrap();
+
+                    let command_buffer = cmd_buffer_builder.build().unwrap();
+
+                    let future = previous_frame_end.take().unwrap()
+                        .join(acquire_future) 
+                        .then_execute(queue.clone(), command_buffer).unwrap()
+                        .then_swapchain_present(queue.clone(), swapchain.clone(), image_num)
+                    .then_signal_fence_and_flush();
+
+                    match future {
+                        Ok(future) => { 
+                            previous_frame_end = Some(Box::new(future) as Box<_>); 
+                        }
+
+                        Err(FlushError::OutOfDate) => { 
+                            recreate_swapchain = true; previous_frame_end = Some(Box::new(sync::now(device.clone())) as Box<_>); 
+                        }
+
+                        Err(e) => {
+                            println!("Failed to flush future: {:?}", e);
+                            previous_frame_end = Some(Box::new(sync::now(device.clone())) as Box<_>); 
+                        } 
+                    }
+
+                }, 
+                
+                _ => {} 
+            }
+
+        }); // event_loop end
+
 }
 
-fn window_size_dependent_setup( images: &[Arc<SwapchainImage<Window>>], render_pass: Arc<RenderPass>, dynamic_state: &mut DynamicState 
+fn window_size_dependent_setup( 
+    images: &[Arc<SwapchainImage<Window>>], 
+    render_pass: Arc<RenderPass>, 
+    dynamic_state: &mut DynamicState 
 ) -> Vec<Arc<dyn FramebufferAbstract + Send + Sync>> 
 { 
     let dimensions = images[0].dimensions(); 
